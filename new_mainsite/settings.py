0a3c1e3c2d6a0f0e3208e4b25129d6d6823578df
unknown <vanp2100@gmail.com>
2024-04-26 19:59:32 +0700

    added all

--- new_mainsite/settings.py
+++ new_mainsite/settings.py
@@ -145,7 +145,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 LOGIN_REDIRECT_URL = 'home'
 
-EMAIL_BACKEND = 'django.core.mail.backedns.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
